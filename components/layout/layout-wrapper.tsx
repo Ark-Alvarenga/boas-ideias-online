4f830e83b7e0d2015376fcc0d56af4b8d8618c74
Ark-Alvarenga <arkalvarenga@gmail.com>
2026-03-27 17:05:35 -0300

    landing layout

--- components/layout/layout-wrapper.tsx
+++ components/layout/layout-wrapper.tsx
@@ -6,6 +6,7 @@ export function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   const isDashboard = pathname.startsWith("/dashboard");
+  const isLanding = pathname.startsWith("/landing")
 
-  return <main className={!isDashboard ? "pt-[72px]" : ""}>{children}</main>;
+  return <main className={!isDashboard || !isLanding ? "pt-[72px]" : ""}>{children}</main>;
 }
